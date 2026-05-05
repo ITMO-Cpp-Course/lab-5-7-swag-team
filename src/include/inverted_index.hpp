25a4a0925ae8a2f0f931291c0db5fe0ed2b30e95
ginosab <ginosabath@gmail.com>
2026-05-05 15:17:03 +0300

    changes

--- src/include/inverted_index.hpp
+++ src/include/inverted_index.hpp
@@ -17,7 +17,8 @@ class InvertedIndex
     Result<void> remove(const std::string& name);
     std::vector<std::string> search(const std::string& word) const;
     Result<size_t> count(const std::string& word, const std::string& doc_name) const;
-    const Document* get(const std::string& name) const; // нужен UpdateTransaction, чтобы делать копию и откатывать изменения
+    const Document* get(
+        const std::string& name) const; // нужен UpdateTransaction, чтобы делать копию и откатывать изменения
 
   private:
     using id_t = size_t;
